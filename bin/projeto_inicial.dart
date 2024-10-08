8b820bda2c8e4fb31cd31462e903a64a2a9e6953
Ester Costa de Souza <alwesterc@gmail.com>
2024-10-08 15:21:43 -0300

    tipos primitivos de variáveis do Dart

---
+++ bin/projeto_inicial.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  int idade = 25;
+  double altura = 1.86;
+  bool geek = true;
+  String nome = 'Eter Costa';
+  String apelido = 'theu';
+  String frase = 'Eu sou $apelido \n'
+      'mas meu nome completo é: $nome, \n'
+      'eu me considero geek? $geek. \n'
+      'Eu tenho $altura metros de altura e \n'
+      '$idade anos de idade';
+
+  print(frase);
+}
